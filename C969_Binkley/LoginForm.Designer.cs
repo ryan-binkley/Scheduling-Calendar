8f7058d036651648966a2efc1c664f20b8f3973a
Ryan Binkley <foxtailrb@gmail.com>
2022-12-11 23:25:18 +0000

    Made Log-In forms time panel.

--- C969_Binkley/LoginForm.Designer.cs
+++ C969_Binkley/LoginForm.Designer.cs
@@ -29,20 +29,49 @@ namespace C969_Binkley
 		/// </summary>
 		private void InitializeComponent()
 		{
+            this.timePanel = new System.Windows.Forms.Panel();
+            this.timePanelCurrentTimeLabel = new System.Windows.Forms.Label();
+            this.timePanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // timePanel
+            // 
+            this.timePanel.BackColor = System.Drawing.SystemColors.GrayText;
+            this.timePanel.Controls.Add(this.timePanelCurrentTimeLabel);
+            this.timePanel.Location = new System.Drawing.Point(0, 0);
+            this.timePanel.Name = "timePanel";
+            this.timePanel.Size = new System.Drawing.Size(140, 60);
+            this.timePanel.TabIndex = 0;
+            // 
+            // timePanelCurrentTimeLabel
+            // 
+            this.timePanelCurrentTimeLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.timePanelCurrentTimeLabel.AutoSize = true;
+            this.timePanelCurrentTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timePanelCurrentTimeLabel.Location = new System.Drawing.Point(9, 0);
+            this.timePanelCurrentTimeLabel.Name = "timePanelCurrentTimeLabel";
+            this.timePanelCurrentTimeLabel.Size = new System.Drawing.Size(125, 24);
+            this.timePanelCurrentTimeLabel.TabIndex = 0;
+            this.timePanelCurrentTimeLabel.Text = "Current Time:";
             // 
             // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.timePanel);
             this.Name = "LoginForm";
             this.Text = "Login";
+            this.timePanel.ResumeLayout(false);
+            this.timePanel.PerformLayout();
             this.ResumeLayout(false);
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Panel timePanel;
+        private System.Windows.Forms.Label timePanelCurrentTimeLabel;
+    }
 }
 
