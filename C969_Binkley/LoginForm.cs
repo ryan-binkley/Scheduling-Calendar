d068c1dcadd4bf109025a15cc09b155b3cc860ae
Ryan Binkley <foxtailrb@gmail.com>
2022-12-12 20:55:12 +0000

    Realised I have not commited in a hot minute, created a UserExists function to return true if a user exists.

--- C969_Binkley/LoginForm.cs
+++ C969_Binkley/LoginForm.cs
@@ -15,19 +15,119 @@ namespace C969_Binkley
 {
 	public partial class LoginForm : Form
 	{
+		Calender_Month calendar_month = new Calender_Month();
+		static string user;
+
 		public LoginForm()
 		{
 			InitializeComponent();
 		}
 
+		// Event handler for submit button on LoginForm
         private void submitCredentialsButton_Click(object sender, EventArgs e)
         {
+			if (UserExists())
+            {
+				user = usernameTextbox.Text;
+            }
+
+        }
+
+		// Void -> Boolean
+		// This function returns true if the user in the textbox 'exists' in the database, returns false if it doesn't
+		public bool UserExists()
+        {
+			// Create a local reference to the Sql Connection in the DBConnection class
 			MySqlConnection sqlConnection = DBConnection.sqlConnection;
 
-			if (sqlConnection.State == ConnectionState.Open)
+			try
             {
-				MessageBox.Show("It is open");
+				// If the connection is not open, inform user and return false to get out of the function call
+				if (!(sqlConnection.State == ConnectionState.Open))
+				{
+					MessageBox.Show("Connection to Database is closed.", "Connection Error");
+					return false;
+				}
+				
+				// Create new instance of MySqlCommand with the SqlCmd and the SqlConnection as parameters
+				MySqlCommand mySqlCmd = new MySqlCommand("SELECT userName FROM user", sqlConnection);
+
+				// Create a SqlDataReader to execute and read the output of the SqlCommand
+				MySqlDataReader sqlReader = mySqlCmd.ExecuteReader();
+
+				// Create string variable to hold each of the results of the sql command
+				string username;
+
+				// While there is a 'record' after the one it is on, continue to the next record
+				while (sqlReader.Read())
+                {
+					// Set username string to every value of the userName column of the user table
+					username = sqlReader.GetString("userName");
+
+					// If the user exists, close the SqlDataReader and return true
+					if (username.ToUpper() == usernameTextbox.Text.ToUpper())
+					{
+						sqlReader.Close();
+						return true;
+					}
+				}
+				sqlReader.Close();
+
+				// If the code passes all usernames and does not find it, return false
+				return false;
+			}
+
+			// If an error occurs, show a messagebox informing the user of the error and return false
+			catch (MySqlException exception)
+            {
+				MessageBox.Show(exception.Message, "User Exists Error");
+				return false;
             }
-        }
+		}
+
+		public string GrabPassword()
+        {
+			// Create a local reference to the Sql Connection in the DBConnection class
+			MySqlConnection sqlConnection = DBConnection.sqlConnection;
+
+			try
+			{
+				// If the connection is not open, inform user and return false to get out of the function call
+				if (!(sqlConnection.State == ConnectionState.Open))
+				{
+					MessageBox.Show("Connection to Database is closed.", "Connection Error");
+					return null;
+				}
+
+				// Create new instance of MySqlCommand with the SqlCmd and the SqlConnection as parameters
+				MySqlCommand mySqlCmd = new MySqlCommand("SELECT password FROM user", sqlConnection);
+
+				// Create a SqlDataReader to execute and read the output of the SqlCommand
+				MySqlDataReader sqlReader = mySqlCmd.ExecuteReader();
+
+				// Create string variable to hold each of the results of the sql command
+				string password;
+
+				// While there is a 'record' after the one it is on, continue to the next record
+				while (sqlReader.Read())
+				{
+					
+
+					
+				}
+				sqlReader.Close();
+
+				// If the code passes all usernames and does not find it, return false
+				return null;
+			}
+
+			// If an error occurs, show a messagebox informing the user of the error and return false
+			catch (MySqlException exception)
+			{
+				MessageBox.Show(exception.Message, "User Exists Error");
+				return null;
+			}
+		}
+
     }
 }
