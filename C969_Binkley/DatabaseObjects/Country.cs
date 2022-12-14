48d851361aa5ee1a3a253cc9aef362a33528f7b4
Ryan Binkley <foxtailrb@gmail.com>
2022-12-14 15:00:57 -0700

    Added Country class.

---
+++ C969_Binkley/DatabaseObjects/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969_Binkley.DatabaseObjects
+{
+    class Country
+    {
+        public Country(int countryID, string countryName)
+        {
+            CountryId = countryID;
+            CountryName = countryName;
+        }
+
+        private int countryId;
+        public int CountryId
+        {
+            get { return countryId; }
+            set { countryId = value; }
+        }
+
+        private string country;
+        public string CountryName
+        {
+            get { return country; }
+            set { country = value; }
+        }
+    }
+}
