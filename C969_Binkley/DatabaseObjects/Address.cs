418b2c625df9b6db50c108df4d94f4fd979ec1e7
Ryan Binkley <foxtailrb@gmail.com>
2022-12-14 15:12:00 -0700

    Created phone class.

---
+++ C969_Binkley/DatabaseObjects/Address.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969_Binkley.DatabaseObjects
+{
+    class Address
+    {
+        public Address(int addID, string addName, City inpCity, string inpPhone)
+        {
+            AddressId = addID;
+            AddressName = addName;
+            City = inpCity;
+            Phone = inpPhone;
+        }
+
+        private int addressId;
+        public int AddressId
+        {
+            get { return addressId; }
+            set { addressId = value; }
+        }
+
+        private string address;
+        public string AddressName
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private City city;
+        public City City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+}
