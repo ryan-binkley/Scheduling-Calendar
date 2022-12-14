11d9aaab10115bb826bb72a700e8f93d1cc8c6c1
Ryan Binkley <foxtailrb@gmail.com>
2022-12-14 15:05:17 -0700

    Added city class.

---
+++ C969_Binkley/DatabaseObjects/City.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969_Binkley.DatabaseObjects
+{
+    class City
+    {
+        public City(int cityID, string cityName, Country country)
+        {
+            CityId = cityID;
+            CityName = cityName;
+            Country = country;
+        }
+
+        private int cityId;
+        public int CityId
+        {
+            get { return cityId; }
+            set { cityId = value; }
+        }
+
+        private string city;
+        public string CityName
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        private Country country;
+        public Country Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+    }
+}
