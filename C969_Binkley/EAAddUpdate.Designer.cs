78a287f3e002267a0b57373455056e903a1e5063
Ryan Binkley <foxtailrb@gmail.com>
2022-12-29 11:27:16 -0700

    Set boilerplate for the EAForm.

--- C969_Binkley/EAAddUpdate.Designer.cs
+++ C969_Binkley/EAAddUpdate.Designer.cs
@@ -29,13 +29,41 @@ namespace C969_Binkley
         /// </summary>
         private void InitializeComponent()
         {
+            this.saveButtonEAF = new System.Windows.Forms.Button();
+            this.cancelButtonEAF = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // saveButtonEAF
+            // 
+            this.saveButtonEAF.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.saveButtonEAF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.saveButtonEAF.Location = new System.Drawing.Point(44, 267);
+            this.saveButtonEAF.Name = "saveButtonEAF";
+            this.saveButtonEAF.Size = new System.Drawing.Size(90, 32);
+            this.saveButtonEAF.TabIndex = 35;
+            this.saveButtonEAF.Text = "Save";
+            this.saveButtonEAF.UseVisualStyleBackColor = true;
+            this.saveButtonEAF.Click += new System.EventHandler(this.saveButtonEAF_Click);
+            // 
+            // cancelButtonEAF
+            // 
+            this.cancelButtonEAF.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cancelButtonEAF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cancelButtonEAF.Location = new System.Drawing.Point(180, 267);
+            this.cancelButtonEAF.Name = "cancelButtonEAF";
+            this.cancelButtonEAF.Size = new System.Drawing.Size(90, 32);
+            this.cancelButtonEAF.TabIndex = 36;
+            this.cancelButtonEAF.Text = "Cancel";
+            this.cancelButtonEAF.UseVisualStyleBackColor = true;
+            this.cancelButtonEAF.Click += new System.EventHandler(this.cancelButtonEAF_Click);
             // 
             // EAAddUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(314, 321);
+            this.Controls.Add(this.cancelButtonEAF);
+            this.Controls.Add(this.saveButtonEAF);
             this.Name = "EAAddUpdate";
             this.Text = "EAAddUpdate";
             this.ResumeLayout(false);
@@ -43,5 +71,8 @@ namespace C969_Binkley
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveButtonEAF;
+        private System.Windows.Forms.Button cancelButtonEAF;
     }
 }
--- C969_Binkley/EAAddUpdate.cs
+++ C969_Binkley/EAAddUpdate.cs
@@ -12,9 +12,69 @@ namespace C969_Binkley
 {
     public partial class EAAddUpdate : Form
     {
+        public static string addOrMod = "";
         public EAAddUpdate()
         {
             InitializeComponent();
+        }
+
+        private void saveButtonEAF_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                return;
+            }
+
+
+            if (addOrMod == "add")
+            {
+
+            }
+
+            if (addOrMod == "mod")
+            {
+
+            }
+
+            ClearTextboxes();
+            this.Visible = false;
+            Calender.custForm.Visible = true;
+        }
+
+        private void cancelButtonEAF_Click(object sender, EventArgs e)
+        {
+            ClearTextboxes();
+            this.Visible = false;
+            Calender.custForm.Visible = true;
+        }
+
+        // Void -> Void
+        // This method clears the forms textboxes
+        public void ClearTextboxes()
+        {
+            foreach (Control txtBox in this.Controls)
+            {
+                if (txtBox is TextBox)
+                {
+                    txtBox.Text = "";
+                }
+            }
+        }
+
+        // void -> Boolean
+        // This function ensures that no textboxes are left empty
+        public bool ValidateInput()
+        {
+            foreach (Control txtBox in this.Controls)
+            {
+                if ((txtBox is TextBox) && (txtBox.Text == ""))
+                {
+                    MessageBox.Show("There cannot be any empt inputs!", "Customer Edit Error", MessageBoxButtons.OK);
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
